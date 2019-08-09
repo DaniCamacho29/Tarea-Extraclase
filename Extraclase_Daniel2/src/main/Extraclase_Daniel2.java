e0bdf733de5f696ec8e4867bd880a8a7bc881025
DaniCamacho29 <danicamacho7@hotmail.com>
2019-08-09 01:52:44 -0600

    Segunda tarea extraclase, listo el uso de try y de finally

--- Extraclase_Daniel2/src/main/Extraclase_Daniel2.java
+++ Extraclase_Daniel2/src/main/Extraclase_Daniel2.java
@@ -5,6 +5,9 @@
  */
 package main;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 /**
  *
  * @author Dani Camacho
@@ -16,6 +19,33 @@ public class Extraclase_Daniel2 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        
+        Scanner scanner = new Scanner(System.in);
+        int dato1;
+        int dato2;
+        int resultado;
+        
+        System.out.println("Introduzca 2 numeros para dividirlos ");
+        
+        
+        try{ System.out.println("Digita el primer numero: ");
+             dato1 = scanner.nextInt();
+             System.out.println("Digita el segundo numero: ");
+             dato2 = scanner.nextInt();
+             resultado = dato1/dato2;
+            
+             System.out.println("La division resultante es: " + resultado);
+            
+        }catch(InputMismatchException e){
+            System.out.println("Los Datos ingresados deben ser numeros enteros"); 
+            
+        }catch(ArithmeticException e){
+            System.out.println("No se puede hacer una division entre 0");
+            
+        }finally{
+            System.out.println("Gracias por la visita");
+        }
     }
-    
 }
+
